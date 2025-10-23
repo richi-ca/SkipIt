94870db0894bf1b06c10cf8c70e2dc2400fa32a0
Richy <ric.castilloa@duocuc.cl>
2025-10-23 16:48:21 -0300

    eliminacion de codigo que impedia refactorizar de manera correcta y el correcto cambio entre algunos componentes web y mobiles

--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { ShoppingCart, User as UserIcon, X } from 'lucide-react';
 import { useCart } from '../context/CartContext';
